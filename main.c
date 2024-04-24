eeb50544ad108b78ec15e5edffd1c8390b019a2c
Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>
2024-04-24 01:20:53 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  int x=3;
+  int b;
+
+  printf("o número é %d\n o dobro de %d é %d \n %d elevado a dois é: %d\n a metade de %d é %d",x,x,x*2,x,b=pow(x,2),x,x/2);
+  return 0;
+}
